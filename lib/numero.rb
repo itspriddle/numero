ab1f55c8043b696ee1fe2541c39ae7b98345b3b3
Joshua Priddle <jpriddle@nevercraft.net>
2010-12-08 03:04:36 -0500

    Fix validations

--- lib/numero.rb
+++ lib/numero.rb
@@ -1,9 +1,10 @@
 module Numero
   VERSION = Version = '0.0.1'
 
-  VALID_US_NUMBER = /^1?([2-9][0-8][0-9])([2-9][0-9]{2})([0-9]{4})$/
+  # See http://en.wikipedia.org/wiki/North_American_Numbering_Plan
+  VALID_PHONE_NUMBER = /^1?([2-9][0-9]{2})([2-9][0-9]{2})([0-9]{4})$/
 
-  def self.valid_us_number?(number)
-    !! number.to_s.match(VALID_US_NUMBER)
+  def self.valid_phone_number?(number)
+    !! number.to_s.match(VALID_PHONE_NUMBER)
   end
 end
--- lib/numero/rails/validations.rb
+++ lib/numero/rails/validations.rb
@@ -8,14 +8,14 @@ module Numero
           if Numero::Rails.major < 3
             validates_presence_of       :number
             validates_numericality_of   :number, :allow_blank => true
-            validates_format_of         :number, :with => Numero::VALID_US_NUMBER
+            validates_format_of         :number, :with => Numero::VALID_PHONE_NUMBER
 
           # Rails 3
           else
             validates :number,
               :presence     => true,
               :numericality => { :allow_blank => true },
-              :format       => { :with => Numero::VALID_US_NUMBER }
+              :format       => { :with => Numero::VALID_PHONE_NUMBER }
 
           end
         end
