660a9c9372717e1690ba34ddc3125cc0e084b95d
Joshua Priddle <jpriddle@nevercraft.net>
2010-12-08 03:14:06 -0500

    Tweak test title

--- test/numero_test.rb
+++ test/numero_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 context 'Numero' do
-  test 'validates a US number' do
+  test 'validates a North American phone number' do
     valid_number   = 15185554488
     valid_number10 = 5185554488
     invalid_number = 11456581535
