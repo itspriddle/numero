c86901b5f16bad205486b16f2d2e2d966f014cf5
Joshua Priddle <jpriddle@nevercraft.net>
2010-12-08 03:06:30 -0500

    Fix method name/tests

--- lib/numero.rb
+++ lib/numero.rb
@@ -4,7 +4,7 @@ module Numero
   # See http://en.wikipedia.org/wiki/North_American_Numbering_Plan
   VALID_PHONE_NUMBER = /^1?([2-9][0-9]{2})([2-9][0-9]{2})([0-9]{4})$/
 
-  def self.valid_phone_number?(number)
+  def self.valid_number?(number)
     !! number.to_s.match(VALID_PHONE_NUMBER)
   end
 end
--- test/numero_test.rb
+++ test/numero_test.rb
@@ -3,11 +3,11 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 context 'Numero' do
   test 'validates a US number' do
     valid_number   = 15185554488
-    valid_number10 = 15185554488
-    invalid_number = 199999999999
+    valid_number10 = 5185554488
+    invalid_number = 11456581535
 
-    assert Numero.valid_us_number?(valid_number)
-    assert Numero.valid_us_number?(valid_number10)
-    assert ! Numero.valid_us_number?(invalid_number)
+    assert Numero.valid_number?(valid_number)
+    assert Numero.valid_number?(valid_number10)
+    assert ! Numero.valid_number?(invalid_number)
   end
 end
